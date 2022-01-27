b23d16a764a8dbdf5b4c19b8dcffcc595e16773c
yiclzh <clare@luanzhu.net>
2022-01-26 20:11:56 -0800

    first commit

---
+++ src/main/java/BaseballElimination.java
@@ -0,0 +1,57 @@
+import edu.princeton.cs.algs4.In;
+
+import java.util.ArrayList;
+
+public class BaseballElimination {
+
+    String[] teams;
+    int[] w;
+    int[] l;
+    int[] r;
+    int[][] g;
+
+
+    public BaseballElimination(String filename) {
+
+    }
+
+    // number of teams
+    public int numberOfTeams() {
+
+    }
+
+    // all teams
+    public Iterable<String> teams() {
+
+    }
+
+    // number of wins for the given team
+    public int wins(String teams) {
+
+    }
+
+    // number of losses for the given team
+    public int losses(String teams) {
+
+    }
+
+    // number of remaining games for the given team
+    public int remaining(String teams) {
+
+    }
+
+    // number of remaining games between team1 and team2
+    public int against(String team1, String team2) {
+
+    }
+
+    // is the given team eliminated?
+    public boolean isEliminated(String team) {
+
+    }
+
+    // subset R of teams that eliminates given team; null if not eliminated
+    public Iterable<String> certificateOfElimination(String team) {
+
+    }
+}
